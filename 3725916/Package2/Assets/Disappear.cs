89ee5baccb92d804fced9305b89d4656d2c96e23
Paul Sinnett <paul.sinnett@gmail.com>
2021-06-24 23:55:14 +0100

    3725916

---
+++ 3725916/Package2/Assets/Disappear.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Disappear : MonoBehaviour
+{
+    [SerializeField] float delay = 1f;
+
+    private void OnCollisionEnter(Collision other)
+    {
+        StartCoroutine(DelayDisappear(delay));
+    }
+
+    IEnumerator DelayDisappear(float _delay)
+    {
+        yield return new WaitForSeconds(_delay);
+        this.gameObject.SetActive(false);
+    }
+
+}
---
+++ 3725916/Package3/Assets/ClickToRemove.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickToRemove : MonoBehaviour
+{
+    Ray ray;
+
+    RaycastHit hit;
+
+    private void Update()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if(Physics.Raycast(ray, out hit, float.MaxValue))
+            {
+                if(hit.rigidbody)
+                {
+                    hit.rigidbody.gameObject.SetActive(false);
+                }
+            }
+        }
+    }
+
+}
