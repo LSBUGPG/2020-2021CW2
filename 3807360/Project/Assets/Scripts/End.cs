7deb2a7b750c24ac6bf0af46aa7c38fd0a1c1c3e
Paul Sinnett <paul.sinnett@gmail.com>
2021-07-01 14:31:24 +0100

    additional project

---
+++ 3807360/Project/Assets/Scripts/End.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class End : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (ScoringSystem.theScore == 21)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+            Cursor.lockState = CursorLockMode.None;
+
+
+        }
+
+
+    }
+}
